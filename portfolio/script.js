2423c2a09d04e77c950b2ab7a6cab14d1c5d6238
AAKASH JANGEETI <114342429+Webdesigner4everyone002@users.noreply.github.com>
2026-03-16 19:59:40 +0530

    Implement image modal functionality
    Added functionality for image modal display and close actions.

--- portfolio/script.js
+++ portfolio/script.js
@@ -12,3 +12,22 @@ behavior:"smooth"
 
 })
 })
+const images = document.querySelectorAll(".achievement-img");
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img=>{
+img.addEventListener("click",()=>{
+modal.style.display="block";
+modalImg.src=img.src;
+});
+});
+
+closeBtn.onclick=()=>{
+modal.style.display="none";
+}
+
+modal.onclick=()=>{
+modal.style.display="none";
+}
