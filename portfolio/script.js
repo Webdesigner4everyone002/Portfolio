9a5411549badb24c789857403fa819ce4c2f4c1d
AAKASH JANGEETI <114342429+Webdesigner4everyone002@users.noreply.github.com>
2026-03-16 16:53:51 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,14 @@
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+link.addEventListener("click", e => {
+
+e.preventDefault();
+
+document.querySelector(link.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+})
+
+})
+})
